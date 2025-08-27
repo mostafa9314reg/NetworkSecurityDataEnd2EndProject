76e05bfda59881b8bd9f9909f8bce8f49a784556
mosttafa9314reg <mostafa9314reg@gmail.com>
2025-08-27 10:58:57 +0330

    adding mlflow tracker to my project

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -25,6 +25,8 @@ from sklearn.ensemble import (
 )
 
 
+import mlflow
+
 
 
 
@@ -36,6 +38,21 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
+
+
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+
 
 
         
@@ -97,6 +114,8 @@ class ModelTrainer:
 
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
+
+        self.track_mlflow(best_model,classification_test_metric)
             
         model_dir_path = os.path.dirname(self.model_trainer_config.trained_model_file_path)
         os.makedirs(model_dir_path,exist_ok=True)
